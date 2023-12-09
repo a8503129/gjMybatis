981a78ea8aef29b780e0f9f781910c3cd3e5ab7a
a8503129 <134759230+a8503129@users.noreply.github.com>
2023-12-09 16:05:21 +0800

    20231209_v1.0

---
+++ gjMybatis/src/main/java/com/example/demo/mapper/porderMapper.java
@@ -0,0 +1,26 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.vo.porder;
+
+@Mapper //產生bean
+public interface porderMapper {
+	//create
+	@Insert("insert into porder(name,a,b,c) values(#{name},#{a},#{b},#{c})")
+	void add(porder p);
+	
+	//read
+	@Select("select * from porder")
+	List<porder> selectAll();
+	
+	//update
+	
+	
+	//delete
+
+}
